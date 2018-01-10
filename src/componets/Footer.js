d78541809a3e0507d9d6caa52725ab76b916e9c5
Claudia Zanetel <claudia.zanetel@gmail.com>
2018-01-10 20:48:15 +0100

    footer component

--- src/componets/Footer.js
+++ src/componets/Footer.js
@@ -4,6 +4,15 @@ class Footer extends Component {
   render() {
     return (
       <div className="row">
+        <div className="col-sm-12 footer-box">
+          <div className="col-sm-12 footer-content">
+            <h5>Studio di Massoterapia di Eleonora Orler</h5>
+            <p>P.IVA</p>
+            <p>Via, 38050 Imer (TN)</p>
+            <p><a href="mailto:eleonora.orler@gmail.com">eleonora.orler@gmail.com</a></p>
+            <p><a href="tel:+393487465813">3487465813</a></p>
+          </div>
+        </div>
       </div>
     );
   }

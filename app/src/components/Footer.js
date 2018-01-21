f983214bac903eaeb9af0bcc83f7f3b0ebb64c84
Claudia Zanetel <claudia.zanetel@gmail.com>
2018-01-21 14:16:40 +0100

    added css for footer and resized the menu div

--- app/src/components/Footer.js
+++ app/src/components/Footer.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 
+import './Footer.css';
+
 class Footer extends Component {
   render() {
     return (
       <div className="row">
         <div className="col-sm-12 footer-box">
           <div className="col-sm-12 footer-content">
-            <h5>Studio di Massoterapia di Eleonora Orler</h5>
+            <h5 className="footerTitle">Studio di Massoterapia di Eleonora Orler</h5>
             <p>P.IVA 02432570220</p>
             <p>Via Guglielmo Marconi 1/1, 38050 Imer (TN)</p>
             <p><a href="mailto:ele.orler@gmail.com">ele.orler@gmail.com</a></p>

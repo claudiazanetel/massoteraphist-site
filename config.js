2d27f02c9ad3604671eba8dfbde2a01bb3df1a9a
Claudia Zanetel <claudia.zanetel@gmail.com>
2018-01-23 22:16:31 +0100

    added env variables

---
+++ config.js
@@ -0,0 +1,3 @@
+exports.calendar_id = process.env.CALENDAR_ID;
+exports.api_key = process.env.API_KEY;
+exports.api_key_map = process.env.API_KEY_MAP;
--- server.js
+++ server.js
@@ -1,11 +1,13 @@
+if (process.env.ENVIRONMENT !== 'production') {
+  require('dotenv').config();
+}
+
 const express = require('express');
 // const path = require('path');
 const app = express();
 
 const googleCalendar = require('./GoogleCalendarEvents');
 const API = require('./config');
-
-// app.use('/', express.static(path.join(__dirname, 'UI')))
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
@@ -25,4 +27,5 @@ app.get('/api/googleMap', (req, res) => {
 app.listen(3030, function () {
   console.log('Example app listening on port 3030!');
 });
+
 
